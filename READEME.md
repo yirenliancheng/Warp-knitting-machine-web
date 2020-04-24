5ed5048f15c692c1665621ec32996b8023210fe1
yirenliancheng <761101850@qq.com>
2020-04-24 22:37:00 +0800

    webpack管理图片、CSS

--- READEME.md
+++ READEME.md
@@ -1,5 +1,14 @@
-#1.涉及的技术内容
+# 1.涉及的技术内容
   Webpack
   Vue
   Bootstrap
   Git
+# 2.需要安装的模块
+   加载CSS资源：安装style-loader和css-loader
+       npm install --save-dev style-loader css-loader
+   加载图片资源：安装file-loader 和url-loader
+       npm install --save-dev file-loader
+# 3.辅助插件
+   清空dist文件夹的插件：clean-webpack-plugin
+       npm install clean-webpack-plugin --save-dev
+   小心使用，会把index.html也给清除的
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,46 @@
 const path = require('path');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+
 
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {//处理不了HTML中的img图片
+        test: /\.(png|svg|jpg|gif)$/,
+        loader: 'url-loader',//一个处理器可以不用数组use，用loader就行
+        options: {
+          //给url添加路径修饰。如果index.html在dist文件夹中，则不用添加publicPath
+          publicPath: '/研究生课题/knitting-machine/dist/',
+          //图片大小小于8kb，就会被base64处理 优点：减少请求数量（减轻服务器压力） 缺点：图片体积会更大（文件请求速度更慢）
+          limit: 8 * 1024,
+          //url-loader默认使用es6模块化解析，html-loader默认使用commonjs引入图片
+          //解析时会出现问题：[module Object]
+          //解决方法：关闭url-loader的es6模块化，使用commonjs解析
+          esModule: false,
+          //给图片重命名  取原来哈希值名字的前十位，ext为原来图片的格式
+          name: '[hash:10].[ext]'
+        }
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader'
+      }
+    ]
+  },
+  plugins:[
+    new CleanWebpackPlugin()
+  ],
+  mode: 'development'
 };
